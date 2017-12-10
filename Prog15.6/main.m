ec992ff38a24aa9baf7c6b5e25c14b4c1b455dfc
admin <dangtinhtong@gmail.com>
2017-12-10 07:13:55 +0700

    First commit

--- Prog15.6/main.m
+++ Prog15.6/main.m
@@ -7,11 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Foundation/NSAutoreleasePool.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        int i;
+
+        
+        // Create  an arry to contain the month names
+        NSArray *monthNames= [NSArray arrayWithObjects:@"January",@"February",@"Macrch",@"Aprill",@"May",@"June"@"August",@"Setember",@"October",@"November",@"December", nil ];
+    // List all the elements in the arry
+        NSLog(@"  Month   Name");
+        NSLog(@" ======= ======");
+        for(i=0; i<12; ++i)
+            
+        //for (int j =0; j<12; ++j)
+        
+        
+            NSLog(@" %2i    %@",i+1,[monthNames objectAtIndex:i]);
+        
+                                     
     }
     return 0;
 }
